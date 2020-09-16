255ee21031f1e05e0e9164dddd2126458cfe95dd
tinybitturtle@gmail.com <tinybitturtle@gmail.com>
2020-09-16 09:20:25 -0700

    start from scratch the gems package

---
+++ Runtime/ActionCtrl.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Object = System.Object;
+
+namespace TinyBitTurtle
+{
+    public partial class  ActionCtrl : SingletonMonoBehaviour<ActionCtrl>
+    {
+        public void DebugOutput(string actionName) { Debug.LogError(CRC32.Compute(actionName) + "/*" + actionName + "*/");}
+    }
+}
---
+++ Runtime/CameraShakeCtrl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CameraShakeCtrl : SingletonMonoBehaviour<CameraShakeCtrl>
+{
+    public float duration;
+    public float amplitude;
+
+    private float timer = 0;
+    private Vector3 shake = new Vector3();
+
+    public void StartShake()
+    {
+        timer = duration;
+
+        // add sound
+    }
+    // Use this for initialization
+    private void Start()
+    {
+        shake.z = gameObject.transform.position.z;
+        StartCoroutine("Shake");
+    }
+
+    // Update is called once per frame
+    IEnumerator Shake()
+    {
+        while (true)
+        {
+            if (timer > 0)
+            {
+                float normalizedAmplitude = amplitude * (timer / duration);
+                shake.x = Random.Range(-normalizedAmplitude, normalizedAmplitude);
+                shake.y = Random.Range(-normalizedAmplitude, normalizedAmplitude);
+
+                gameObject.transform.position = shake;
+
+                timer -= Time.deltaTime;
+                if (timer < 0.0f)
+                    timer = 0.0f;
+            }
+
+            yield return null;
+        }
+    }
+}
